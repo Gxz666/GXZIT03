08e4887c74110e6e4afe7393460c68cb26ffaace
Gxz666 <mac@MacdeiMac-78.local>
2016-03-11 22:52:11 -0800

    GXZ666
    WO SHI GXZ

---
+++ IT03/IT03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IT03
+//
+//  Created by Mac on 16/3/11.
+//  Copyright © 2016年 Gxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IT03/IT03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IT03
+//
+//  Created by Mac on 16/3/11.
+//  Copyright © 2016年 Gxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
