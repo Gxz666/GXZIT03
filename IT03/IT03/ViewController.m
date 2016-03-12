3f8b2d522e2e0ede7ce794e0047b3c4eabbd990e
Gxz666 <mac@MacdeiMac-78.local>
2016-03-11 23:12:43 -0800

    Revert "Revert "Revert "GXZ666"""
    This reverts commit ced154aa20c3d345d302c253b6552ef47c64cd0a.

--- IT03/IT03/ViewController.m
+++ IT03/IT03/ViewController.m
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    NSString  *STR =  @"123";
 }
 
 - (void)didReceiveMemoryWarning {
